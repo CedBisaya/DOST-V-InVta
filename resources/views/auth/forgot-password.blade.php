27213500c647468030afa03702135e17e3721d9a
frahahahancis <fgdc2022-3856-92793@bicol-u.edu.ph>
2026-03-11 09:11:46 +0800

    kunting revisions ulit sa login

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -2,7 +2,7 @@
 
     <div class="mx-auto w-full max-w-[320px] sm:max-w-[500px] mb-4">
         <a href="{{ route('login') }}" class="inline-flex items-center text-xs sm:text-sm font-bold text-gray-400 hover:text-[#00AEEF] transition-colors group">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 pb-1">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
             </svg>
 
